e630f941791b94fb58b4561a551028c8b7fc3928
Milton Brandes <miltonbrandess@hotmail.com>
2016-03-31 22:53:02 -0300

    primer commit mio probando el gitignore

---
+++ prueba2/src/prueba2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int i = 0;
+	for(i; i < 10; i++)
+	{
+		printf("el numero es: %d",i);
+	}
+	return EXIT_SUCCESS;
+}
